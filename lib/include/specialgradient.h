40024e4aae893c4b5cef9e9d2f951544507af855
Oestreich, Lennart <lennart.oestreich@stud.tu-darmstadt.de>
2020-10-16 13:59:56 +0200

    Feat/special-gradient

---
+++ lib/include/specialgradient.h
@@ -0,0 +1,16 @@
+#ifndef CUAS_SPECIALGRADIENT_H
+#define CUAS_SPECIALGRADIENT_H
+
+#include "PetscGrid.h"
+
+namespace CUAS {
+
+// PetscGrid h and PetscGrid gradient need to be created beforehand. The gradient-grid needs to be of the same size as
+// the grid h. The same needs to be respected for the calculation of gradient2_central.
+void gradient2(PetscGrid *const h, PetscGrid *const gradient, PetscScalar const dx);
+
+void gradient2_central(PetscGrid *const h, PetscGrid *const gradient, PetscScalar const dx);
+
+}  // namespace CUAS
+
+#endif
