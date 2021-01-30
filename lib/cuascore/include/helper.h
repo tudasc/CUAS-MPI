a4acdb4c2cfa4737208e07718d5877b2b8b7b6d4
Emunds, Raban <raban.emunds@stud.tu-darmstadt.de>
2021-01-30 19:54:36 +0100

    adds Feat/solver loop

--- lib/cuascore/include/helper.h
+++ lib/cuascore/include/helper.h
@@ -50,6 +50,39 @@ inline void overburdenPressure(PetscGrid &result, PetscGrid &thk) {
   thk.restoreGlobal2dArr(thk2d);
 }
 
+inline void cavityOpenB(PetscGrid &result, PetscScalar const beta, PetscScalar const v_b, PetscGrid &K) {
+  auto resultGlobal = result.getAsGlobal2dArr();
+  auto KGlobal = K.getAsGlobal2dArr();
+  PetscScalar betaVb = beta * v_b;
+  for (int j = 0; j < result.getLocalNumOfRows(); ++j) {
+    for (int i = 0; i < result.getLocalNumOfCols(); ++i) {
+      resultGlobal[j][i] = betaVb * KGlobal[j][i];
+    }
+  }
+  result.setAsGlobal2dArr(resultGlobal);
+  K.restoreGlobal2dArr(KGlobal);
+}
+
+inline void compute_melt(PetscGrid &result, PetscScalar const r, PetscScalar const g, PetscScalar const rho_w,
+                         PetscGrid &T, PetscGrid &K, PetscGrid &gradh2, PetscScalar const rho_i, PetscScalar const L,
+                         PetscScalar const bt) {
+  auto resultGlobal = result.getAsGlobal2dArr();
+  auto KGlobal = K.getAsGlobal2dArr();
+  auto TGlobal = T.getAsGlobal2dArr();
+  auto gradh2Global = gradh2.getAsGlobal2dArr();
+  PetscScalar r_g_rhow = r * g * rho_w;
+  PetscScalar rhoi_L = rho_i * L;
+  for (int j = 0; j < result.getLocalNumOfRows(); ++j) {
+    for (int i = 0; i < result.getLocalNumOfCols(); ++i) {
+      resultGlobal[j][i] = r_g_rhow * TGlobal[j][i] * KGlobal[j][i] * gradh2Global[j][i] / rhoi_L;
+    }
+  }
+  result.setAsGlobal2dArr(resultGlobal);
+  K.restoreGlobal2dArr(KGlobal);
+  T.restoreGlobal2dArr(TGlobal);
+  gradh2.restoreGlobal2dArr(gradh2Global);
+}
+
 }  // namespace CUAS
 
 #endif
