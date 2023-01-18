fa91fdc9c856c282abbd1f7568370731f9fb68ca
Fischler, Yannic <yannic.fischler@tu-darmstadt.de>
2023-01-18 18:30:26 +0100

    applies cmake format and adds automatic check in CI

--- cmake/cxxopts.cmake
+++ cmake/cxxopts.cmake
@@ -6,22 +6,24 @@ if(EXISTS ${CXXOPTS_INCLUDE})
   add_custom_target(cxxopts)
 else()
   # in lib/cuascore/src/CUASArgs.cpp: #include "cxxopts.hpp""
-  find_path(CXXOPTS_INCLUDE cxxopts.hpp HINTS ${CMAKE_CURRENT_SOURCE_DIR}/extern/cxxopts/include)
+  find_path(CXXOPTS_INCLUDE cxxopts.hpp
+            HINTS ${CMAKE_CURRENT_SOURCE_DIR}/extern/cxxopts/include)
   if(EXISTS ${CXXOPTS_INCLUDE})
     message(STATUS "CXXOPTS_INCLUDE found, skipping rebuild")
     add_custom_target(cxxopts)
   else()
-    message(STATUS "CXXOPTS_INCLUDE not found, download into extern during make")
-    ExternalProject_Add(cxxopts
-      SOURCE_DIR          ${CMAKE_CURRENT_SOURCE_DIR}/extern/cxxopts
-      GIT_REPOSITORY      "https://github.com/jarro2783/cxxopts.git"
-      GIT_TAG             v2.2.1
-      CONFIGURE_COMMAND   ""
-      BUILD_COMMAND       ""
-      INSTALL_COMMAND     ""
-      TEST_COMMAND        ""
-      GIT_SHALLOW         true
-    )
+    message(
+      STATUS "CXXOPTS_INCLUDE not found, download into extern during make")
+    ExternalProject_Add(
+      cxxopts
+      SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/extern/cxxopts
+      GIT_REPOSITORY "https://github.com/jarro2783/cxxopts.git"
+      GIT_TAG v2.2.1
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      INSTALL_COMMAND ""
+      TEST_COMMAND ""
+      GIT_SHALLOW true)
     set(CXXOPTS_INCLUDE ${CMAKE_CURRENT_SOURCE_DIR}/extern/cxxopts/include)
   endif()
 endif()
@@ -29,11 +31,7 @@ endif()
 message(STATUS "CXXOPTS_INCLUDE ${CXXOPTS_INCLUDE}")
 
 function(add_cxxopts target)
-  add_dependencies(${target}
-    cxxopts
-  )
+  add_dependencies(${target} cxxopts)
 
-  target_include_directories(${target} SYSTEM PUBLIC
-    ${CXXOPTS_INCLUDE}
-  )
+  target_include_directories(${target} SYSTEM PUBLIC ${CXXOPTS_INCLUDE})
 endfunction()
--- cmake/spdlog.cmake
+++ cmake/spdlog.cmake
@@ -6,22 +6,23 @@ if(EXISTS ${SPDLOG_INCLUDE})
   add_custom_target(spdlog)
 else()
   # in lib/petscwrapper/include/Logger.h: #include "spdlog/spdlog.h"
-  find_path(SPDLOG_INCLUDE spdlog/spdlog.h HINT ${CMAKE_CURRENT_SOURCE_DIR}/extern/spdlog/include)
+  find_path(SPDLOG_INCLUDE spdlog/spdlog.h HINT
+            ${CMAKE_CURRENT_SOURCE_DIR}/extern/spdlog/include)
   if(EXISTS ${SPDLOG_INCLUDE})
     message(STATUS "SPDLOG_INCLUDE found, skipping rebuild")
     add_custom_target(spdlog)
   else()
     message(STATUS "SPDLOG library not found, download into extern during make")
-    ExternalProject_Add(spdlog
-      SOURCE_DIR          ${CMAKE_CURRENT_SOURCE_DIR}/extern/spdlog
-      GIT_REPOSITORY      "https://github.com/gabime/spdlog.git"
-      GIT_TAG             v1.x
-      CONFIGURE_COMMAND   ""
-      BUILD_COMMAND       ""
-      INSTALL_COMMAND     ""
-      TEST_COMMAND        ""
-      GIT_SHALLOW         true
-    )
+    ExternalProject_Add(
+      spdlog
+      SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/extern/spdlog
+      GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+      GIT_TAG v1.x
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      INSTALL_COMMAND ""
+      TEST_COMMAND ""
+      GIT_SHALLOW true)
     set(SPDLOG_INCLUDE ${CMAKE_CURRENT_SOURCE_DIR}/extern/spdlog/include)
   endif()
 endif()
@@ -29,11 +30,7 @@ endif()
 message(STATUS "SPDLOG_INCLUDE ${SPDLOG_INCLUDE}")
 
 function(add_spdlog target)
-  add_dependencies(${target}
-    spdlog
-  )
+  add_dependencies(${target} spdlog)
 
-  target_include_directories(${target} SYSTEM PUBLIC
-    ${SPDLOG_INCLUDE}
-  )
+  target_include_directories(${target} SYSTEM PUBLIC ${SPDLOG_INCLUDE})
 endfunction()
