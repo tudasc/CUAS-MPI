tudasc/CUAS-MPI

commit f771d8695119810adbf13cccfc4c7777c781cff6

refactors forcings: - removes default parameter in `getCurrentQ`, according to clang-tidy - defines all constructors explicitly - separates declarations and definitions - unifies implementation and usage of `applyMultiplier` and `applyOffset`

lib/cuascore/include/Forcing/Forcing.h
@@ -15,8 +15,14 @@ namespace CUAS {
 
 class Forcing {
  public:
-  virtual PETScGrid const &getCurrentQ(timeSecs currTime = 0) = 0;
-  virtual ~Forcing() = default;
+  explicit Forcing() = default;
+  Forcing(const Forcing &) = delete;
+  Forcing &operator=(Forcing const &) = delete;
+  Forcing(const Forcing &&) = delete;
+  Forcing &operator=(Forcing const &&) = delete;
+  ~Forcing() = default;
+
+  virtual PETScGrid const &getCurrentQ(timeSecs currTime) = 0;
 
  private:
   virtual void applyMultiplier(PetscScalar multiplier) = 0;

commit e6ea43fe51f8759689f79aaf07e6ac7223746d83

fix: * memory leak in forcing, caused by f771d86 (missing virtual destructor)
major refactorings:
* Forcing/*

minor refactorings:
* ModelReader
* CUASKernels
* CUASArgs
* utils

lib/cuascore/include/CUASKernels.h
@@ -235,16 +235,22 @@ inline void binaryDilation(PETScGrid &output, PETScGrid const &input) {
 
   for (int i = 0; i < iter_rows; ++i) {
     for (int j = 0; j < iter_cols; ++j) {
-      if (in(index_rows, index_cols, GHOSTED))
+      // TODO we only have to evaluate this until we found one true
+      if (0.0 != in(index_rows, index_cols, GHOSTED)) {
         out(i, j) = true;
-      if (in(index_rows + 1, index_cols, GHOSTED))
+      }
+      if (0.0 != in(index_rows + 1, index_cols, GHOSTED)) {
         out(i, j) = true;
-      if (in(index_rows - 1, index_cols, GHOSTED))
+      }
+      if (0.0 != in(index_rows - 1, index_cols, GHOSTED)) {
         out(i, j) = true;
-      if (in(index_rows, index_cols + 1, GHOSTED))
+      }
+      if (0.0 != in(index_rows, index_cols + 1, GHOSTED)) {
         out(i, j) = true;
-      if (in(index_rows, index_cols - 1, GHOSTED))
+      }
+      if (0.0 != in(index_rows, index_cols - 1, GHOSTED)) {
         out(i, j) = true;
+      }
       ++index_cols;
     }
     index_cols = 1;

lib/cuascore/include/Forcing/BufferedForcing.h
@@ -31,7 +31,7 @@ class BufferedForcing : public Forcing {
   BufferedForcing &operator=(BufferedForcing const &) = delete;
   BufferedForcing(const BufferedForcing &&) = delete;
   BufferedForcing &operator=(BufferedForcing const &&) = delete;
-  ~BufferedForcing() = default;
+  ~BufferedForcing() override = default;
 
   // member functions
  public:
@@ -41,8 +41,6 @@ class BufferedForcing : public Forcing {
  public:
   // member
  private:
-  std::unique_ptr<PETScGrid> currQ;
-
   std::vector<timeSecs> allTime;
   std::vector<timeSecs> time;
   std::vector<std::unique_ptr<PETScGrid>> forcingStack;
@@ -136,7 +134,7 @@ inline BufferedForcing::BufferedForcing(std::unique_ptr<NetCDFFile> &ncFile, std
   }
 
   // resize time and forcingStack as a buffer
-  time.resize(numberOfSlicesPerLoad, 0.0);
+  time.resize(numberOfSlicesPerLoad, 0);
   forcingStack.resize(numberOfSlicesPerLoad);
 
   for (std::unique_ptr<PETScGrid> &grid : forcingStack) {

lib/cuascore/include/Forcing/Forcing.h
@@ -20,10 +20,18 @@ class Forcing {
   Forcing &operator=(Forcing const &) = delete;
   Forcing(const Forcing &&) = delete;
   Forcing &operator=(Forcing const &&) = delete;
-  ~Forcing() = default;
+  virtual ~Forcing() = default;
 
+  // member functions
+ public:
   virtual PETScGrid const &getCurrentQ(timeSecs currTime) = 0;
 
+  // member
+ public:
+  // member
+ protected:
+  std::unique_ptr<PETScGrid> currQ;
+  // member functions
  private:
   virtual void applyMultiplier(PetscScalar multiplier) = 0;
   virtual void applyOffset(PetscScalar offset) = 0;

lib/cuascore/include/Forcing/ScalarTimeDependentForcing.h
@@ -31,16 +31,26 @@ class ScalarTimeDependentForcing : public Forcing {
   ScalarTimeDependentForcing &operator=(ScalarTimeDependentForcing const &) = delete;
   ScalarTimeDependentForcing(const ScalarTimeDependentForcing &&) = delete;
   ScalarTimeDependentForcing &operator=(ScalarTimeDependentForcing const &&) = delete;
-  ~ScalarTimeDependentForcing() = default;
+  ~ScalarTimeDependentForcing() override = default;
 
+  // member functions
+ public:
   PETScGrid const &getCurrentQ(timeSecs currTime) override;
 
+  // member
+ public:
+  // member
  private:
-  std::unique_ptr<PETScGrid> currQ;
   std::vector<timeSecs> const time;
   std::vector<PetscScalar> timeSeries;
+
   bool const loopForcing;
-  int rowIndex, colIndex;
+  int const rowIndex;
+  int const colIndex;
+
+  // member functions
+ private:
+  PETScGrid const &setValueAtPos(PetscScalar value);
 
   // Note, we apply the multiplier directly on the scalar time series input.
   void applyMultiplier(PetscScalar multiplier) override {
@@ -59,8 +69,6 @@ class ScalarTimeDependentForcing : public Forcing {
     std::transform(this->timeSeries.begin(), this->timeSeries.end(), this->timeSeries.begin(),
                    [&offset](auto &el) { return el + offset; });
   }
-
-  PETScGrid const &setValueAtPos(PetscScalar value);
 };
 
 inline ScalarTimeDependentForcing::ScalarTimeDependentForcing(int nx, int const ny, int rowIndex, int colIndex,

lib/cuascore/include/Forcing/SteadyForcing.h
@@ -17,36 +17,42 @@ namespace CUAS {
 
 class SteadyForcing : public Forcing {
  public:
-  explicit SteadyForcing(PETScGrid const &m_forcing, PetscScalar const multiplier = 1.0, PetscScalar const offset = 0.0)
-      : forcing(m_forcing.getTotalNumOfCols(), m_forcing.getTotalNumOfRows()) {
-    forcing.copy(m_forcing);
+  explicit SteadyForcing(PETScGrid const &m_forcing, PetscScalar const multiplier = 1.0,
+                         PetscScalar const offset = 0.0) {
+    currQ = std::make_unique<PETScGrid>(m_forcing.getTotalNumOfCols(), m_forcing.getTotalNumOfRows());
+    currQ->copy(m_forcing);
 
     SteadyForcing::applyMultiplier(multiplier);
     SteadyForcing::applyOffset(offset);
-  };
+  }
   SteadyForcing(const SteadyForcing &) = delete;
   SteadyForcing &operator=(SteadyForcing const &) = delete;
   SteadyForcing(const SteadyForcing &&) = delete;
   SteadyForcing &operator=(SteadyForcing const &&) = delete;
-  ~SteadyForcing() = default;
+  ~SteadyForcing() override = default;
 
-  PETScGrid const &getCurrentQ(timeSecs /*currTime*/) override { return forcing; }
+  // member functions
+ public:
+  PETScGrid const &getCurrentQ(timeSecs /*currTime*/) override { return *currQ; }
 
+  // member
+ public:
+  // member
  private:
-  PETScGrid forcing;
-
+  // member functions
+ private:
   void applyMultiplier(PetscScalar multiplier) override {
     if (multiplier == 1.0) {
       return;
     }
-    forcing.applyMultiplier(multiplier);
+    currQ->applyMultiplier(multiplier);
   }
 
   void applyOffset(PetscScalar offset) override {
     if (offset == 0.0) {
       return;
     }
-    forcing.applyOffset(offset);
+    currQ->applyOffset(offset);
   }
 };
 

lib/cuascore/include/Forcing/TimeDependentForcing.h
@@ -31,15 +31,26 @@ class TimeDependentForcing : public Forcing {
   TimeDependentForcing &operator=(TimeDependentForcing const &) = delete;
   TimeDependentForcing(const TimeDependentForcing &&) = delete;
   TimeDependentForcing &operator=(TimeDependentForcing const &&) = delete;
-  ~TimeDependentForcing() = default;
+  ~TimeDependentForcing() override = default;
 
+  // member functions
+ public:
   PETScGrid const &getCurrentQ(timeSecs currTime) override;
 
+  // member
+ public:
+  // member
  private:
   std::vector<timeSecs> const time;
   std::vector<std::unique_ptr<PETScGrid>> forcingStack;
+
+  PetscScalar const multiplier;
+  PetscScalar const offset;
   bool const loopForcing;
-  std::unique_ptr<PETScGrid> currQ;
+
+  // member functions
+ private:
+  void loadSlices(std::vector<std::unique_ptr<PETScGrid>> &forcing);
 
   void applyMultiplier(PetscScalar multiplier) override {
     if (multiplier == 1.0) {
@@ -63,7 +74,7 @@ class TimeDependentForcing : public Forcing {
 inline TimeDependentForcing::TimeDependentForcing(std::vector<std::unique_ptr<PETScGrid>> &forcing,
                                                   std::vector<timeSecs> const &time, PetscScalar multiplier,
                                                   PetscScalar offset, bool loopForcing)
-    : time(time), loopForcing(loopForcing) {
+    : time(time), multiplier(multiplier), offset(offset), loopForcing(loopForcing) {
   if (time.size() != forcing.size()) {
     CUAS_ERROR("{}: time and forcing sizes are not compatible. Exiting.", __PRETTY_FUNCTION__)
     exit(1);
@@ -78,11 +89,9 @@ inline TimeDependentForcing::TimeDependentForcing(std::vector<std::unique_ptr<PE
     exit(1);
   }
 
-  currQ = std::make_unique<PETScGrid>(forcing[0]->getTotalNumOfCols(), forcing[0]->getTotalNumOfRows());
-  std::move(begin(forcing), end(forcing), std::back_inserter(forcingStack));
+  loadSlices(forcing);
 
-  TimeDependentForcing::applyMultiplier(multiplier);
-  TimeDependentForcing::applyOffset(offset);
+  currQ = std::make_unique<PETScGrid>(forcingStack[0]->getTotalNumOfCols(), forcingStack[0]->getTotalNumOfRows());
 }
 
 inline PETScGrid const &TimeDependentForcing::getCurrentQ(timeSecs currTime) {
@@ -134,6 +143,14 @@ inline PETScGrid const &TimeDependentForcing::getCurrentQ(timeSecs currTime) {
     }
   }
   return *currQ;
+}
+
+inline void TimeDependentForcing::loadSlices(std::vector<std::unique_ptr<PETScGrid>> &forcing) {
+  forcingStack.reserve(forcing.size());
+  std::move(begin(forcing), end(forcing), std::back_inserter(forcingStack));
+
+  TimeDependentForcing::applyMultiplier(multiplier);
+  TimeDependentForcing::applyOffset(offset);
 }
 
 }  // namespace CUAS

lib/cuascore/include/ModelReader.h
@@ -35,30 +35,30 @@ class ModelReader {
 
  public:
   explicit ModelReader(std::string const &fileName);
-  std::unique_ptr<CUAS::CUASModel> fillModelFromNetcdf();
-  static void restartFromFile(CUAS::CUASSolver &solver, std::string const &restartFile,
-                              bool restartNoneZeroInitialGuess);
+  std::unique_ptr<CUASModel> fillModelFromNetcdf();
+  static void restartFromFile(CUASSolver &solver, std::string const &restartFile, bool restartNoneZeroInitialGuess);
 
-  static std::unique_ptr<CUAS::TimeDependentForcing> getTimeDependentForcing(
+  static std::unique_ptr<TimeDependentForcing> getTimeDependentForcing(
       std::string const &ncFileName, std::string const &variableName, std::vector<PetscScalar> const &xAxis,
       std::vector<PetscScalar> const &yAxis, PetscScalar multiplier = 1.0, PetscScalar offset = 0.0,
       bool loopForcing = false);
 
-  static std::unique_ptr<CUAS::BufferedForcing> getBufferedForcing(
-      std::string const &ncFileName, std::string const &variableName, std::vector<PetscScalar> const &xAxis,
-      std::vector<PetscScalar> const &yAxis, int numberOfSlicesPerLoad, PetscScalar multiplier = 1.0,
-      PetscScalar offset = 0.0, bool loopForcing = false);
+  static std::unique_ptr<BufferedForcing> getBufferedForcing(std::string const &ncFileName,
+                                                             std::string const &variableName,
+                                                             std::vector<PetscScalar> const &xAxis,
+                                                             std::vector<PetscScalar> const &yAxis,
+                                                             int numberOfSlicesPerLoad, PetscScalar multiplier = 1.0,
+                                                             PetscScalar offset = 0.0, bool loopForcing = false);
 
-  static std::unique_ptr<CUAS::ScalarTimeDependentForcing> getScalarTimeDependentForcing(
+  static std::unique_ptr<ScalarTimeDependentForcing> getScalarTimeDependentForcing(
       std::string const &ncFileName, std::string const &variableName, std::vector<PetscScalar> const &xAxis,
       std::vector<PetscScalar> const &yAxis, PetscScalar multiplier = 1.0, PetscScalar offset = 0.0,
       bool loopForcing = false);
 
-  static std::unique_ptr<CUAS::SteadyForcing> getSteadyForcing(std::string const &ncFileName,
-                                                               std::string const &variableName,
-                                                               std::vector<PetscScalar> const &xAxis,
-                                                               std::vector<PetscScalar> const &yAxis,
-                                                               PetscScalar multiplier = 1.0, PetscScalar offset = 0.0);
+  static std::unique_ptr<SteadyForcing> getSteadyForcing(std::string const &ncFileName, std::string const &variableName,
+                                                         std::vector<PetscScalar> const &xAxis,
+                                                         std::vector<PetscScalar> const &yAxis,
+                                                         PetscScalar multiplier = 1.0, PetscScalar offset = 0.0);
 
   static bool isTimeDependent(std::string const &ncFileName, std::string const &variableName);
 

lib/cuascore/include/timeparse.h
@@ -159,7 +159,6 @@ inline std::vector<timeSecs> getTimeStepArray(timeSecs startTime, timeSecs endTi
     exit(1);
   }
 
-  // relationship between beginning and end
   // end has to be larger than start
   if (startTime > endTime) {
     CUAS_ERROR("{} called with invalid startTime = {} > endTime = {}. Exiting.",  //

lib/cuascore/src/CUASArgs.cpp
@@ -38,7 +38,7 @@ void parseArgs(int argc, char **argv, CUASArgs &args) {
 
   if (args.verbose) {
     CUAS_INFO_RANK0("CUASArgs.cpp: parseArgs:\n\tinput: {}\n\toutput: {}.", result["input"].as<std::string>(),
-                    result["output"].as<std::string>());
+                    result["output"].as<std::string>())
   }
 }
 
@@ -77,7 +77,7 @@ inline void defineArgs(cxxopts::Options &options) {
        "The last point in time (starttime + totaltime == endtime). Use either totaltime or endtime. Example: --endtime '3 years 1 week'",
        cxxopts::value<std::string>()->default_value(""))
       ("dt",
-       "Time step length. Example: --dt '12 hours', --dt 1 day",
+       "Time step length. Example: --dt '12 hours' or --dt 1 day",
        cxxopts::value<std::string>()->default_value(""))
       ("timeSteppingTheta",
        "Time stepping family, e.g. theta=1 -> backward Euler, theta=0.5 -> Crank-Nicolson (0 <= theta <= 1)",
@@ -154,13 +154,13 @@ inline void defineArgs(cxxopts::Options &options) {
       ("loopForcing",
        "Loop the forcing when total time is longer than forcing. Otherwise the last step of the forcing is used.")
       ("forcingFile",
-       "forcing input file (netcdf)",
+       "forcing input file (NetCDF)",
        cxxopts::value<std::string>()->default_value(""))
       ("coordinatesFile",
-       "file containing lat/lon grids to copied into the output file (netcdf)",
+       "file containing lat/lon grids to copied into the output file (NetCDF)",
        cxxopts::value<std::string>()->default_value(""))
       ("seaLevelForcing",
-       "Apply sea level forcing from netcdf scalar time series file.",
+       "Apply sea level forcing from NetCDF scalar time series file.",
        cxxopts::value<std::string>()->default_value(""))
       ("positional",
        "Positional arguments: these are the arguments that are entered without an option.",

lib/cuascore/src/ModelReader.cpp
@@ -12,8 +12,8 @@ namespace CUAS {
 
 ModelReader::ModelReader(std::string const &fileName) { file = std::make_unique<NetCDFFile>(fileName, 'r'); }
 
-std::unique_ptr<CUAS::CUASModel> ModelReader::fillModelFromNetcdf() {
-  auto pmodel = std::make_unique<CUAS::CUASModel>(file->getDimX(), file->getDimY());
+std::unique_ptr<CUASModel> ModelReader::fillModelFromNetcdf() {
+  auto pmodel = std::make_unique<CUASModel>(file->getDimX(), file->getDimY());
   auto &model = *pmodel;
 
   file->read("x", model.xAxis);
@@ -28,7 +28,7 @@ std::unique_ptr<CUAS::CUASModel> ModelReader::fillModelFromNetcdf() {
   return pmodel;
 }
 
-void ModelReader::restartFromFile(CUAS::CUASSolver &solver, std::string const &restartFile,
+void ModelReader::restartFromFile(CUASSolver &solver, std::string const &restartFile,
                                   bool restartNoneZeroInitialGuess) {
   auto restartNetcdfFile = std::make_unique<NetCDFFile>(restartFile, 'r');
   restartNetcdfFile->read("head", *solver.currHead);
@@ -39,7 +39,7 @@ void ModelReader::restartFromFile(CUAS::CUASSolver &solver, std::string const &r
   }
 }
 
-std::unique_ptr<CUAS::TimeDependentForcing> ModelReader::getTimeDependentForcing(
+std::unique_ptr<TimeDependentForcing> ModelReader::getTimeDependentForcing(
     std::string const &ncFileName, std::string const &variableName, std::vector<PetscScalar> const &xAxis,
     std::vector<PetscScalar> const &yAxis, PetscScalar multiplier, PetscScalar offset, bool loopForcing) {
   auto ncFile = std::make_unique<NetCDFFile>(ncFileName, 'r');
@@ -66,13 +66,15 @@ std::unique_ptr<CUAS::TimeDependentForcing> ModelReader::getTimeDependentForcing
   ncFile->read("time", time);
   ncFile->read(variableName, forcing);
 
-  return std::make_unique<CUAS::TimeDependentForcing>(forcing, time, multiplier, offset, loopForcing);
+  return std::make_unique<TimeDependentForcing>(forcing, time, multiplier, offset, loopForcing);
 }
 
-std::unique_ptr<CUAS::BufferedForcing> ModelReader::getBufferedForcing(
-    std::string const &ncFileName, std::string const &variableName, std::vector<PetscScalar> const &xAxis,
-    std::vector<PetscScalar> const &yAxis, int numberOfSlicesPerLoad, PetscScalar multiplier, PetscScalar offset,
-    bool loopForcing) {
+std::unique_ptr<BufferedForcing> ModelReader::getBufferedForcing(std::string const &ncFileName,
+                                                                 std::string const &variableName,
+                                                                 std::vector<PetscScalar> const &xAxis,
+                                                                 std::vector<PetscScalar> const &yAxis,
+                                                                 int numberOfSlicesPerLoad, PetscScalar multiplier,
+                                                                 PetscScalar offset, bool loopForcing) {
   auto ncFile = std::make_unique<NetCDFFile>(ncFileName, 'r');
 
   auto nx = xAxis.size();
@@ -86,11 +88,11 @@ std::unique_ptr<CUAS::BufferedForcing> ModelReader::getBufferedForcing(
     exit(1);
   }
 
-  return std::make_unique<CUAS::BufferedForcing>(ncFile, variableName, nx, ny, numberOfSlicesPerLoad, multiplier,
-                                                 offset, loopForcing);
+  return std::make_unique<BufferedForcing>(ncFile, variableName, nx, ny, numberOfSlicesPerLoad, multiplier, offset,
+                                           loopForcing);
 }
 
-std::unique_ptr<CUAS::ScalarTimeDependentForcing> ModelReader::getScalarTimeDependentForcing(
+std::unique_ptr<ScalarTimeDependentForcing> ModelReader::getScalarTimeDependentForcing(
     std::string const &ncFileName, std::string const &variableName, std::vector<PetscScalar> const &xAxis,
     std::vector<PetscScalar> const &yAxis, PetscScalar multiplier, PetscScalar offset, bool loopForcing) {
   // expected dimension names and dimension order. Note, len(x) = 1 and len(y) = 1
@@ -184,15 +186,15 @@ std::unique_ptr<CUAS::ScalarTimeDependentForcing> ModelReader::getScalarTimeDepe
                     yAxis[colIndex], p)
   }
 
-  return std::make_unique<CUAS::ScalarTimeDependentForcing>(nx, ny, rowIndex, colIndex, timeSeries, time, multiplier,
-                                                            offset, loopForcing);
+  return std::make_unique<ScalarTimeDependentForcing>(nx, ny, rowIndex, colIndex, timeSeries, time, multiplier, offset,
+                                                      loopForcing);
 }
 
-std::unique_ptr<CUAS::SteadyForcing> ModelReader::getSteadyForcing(std::string const &ncFileName,
-                                                                   std::string const &variableName,
-                                                                   std::vector<PetscScalar> const &xAxis,
-                                                                   std::vector<PetscScalar> const &yAxis,
-                                                                   PetscScalar multiplier, PetscScalar offset) {
+std::unique_ptr<SteadyForcing> ModelReader::getSteadyForcing(std::string const &ncFileName,
+                                                             std::string const &variableName,
+                                                             std::vector<PetscScalar> const &xAxis,
+                                                             std::vector<PetscScalar> const &yAxis,
+                                                             PetscScalar multiplier, PetscScalar offset) {
   auto ncFile = std::make_unique<NetCDFFile>(ncFileName, 'r');
   // size of the model, not size from the forcing file
   auto nx = (int)xAxis.size();
@@ -209,7 +211,7 @@ std::unique_ptr<CUAS::SteadyForcing> ModelReader::getSteadyForcing(std::string c
 
   PETScGrid forcing(nx, ny);
   ncFile->read(variableName, forcing);
-  return std::make_unique<CUAS::SteadyForcing>(forcing, multiplier, offset);
+  return std::make_unique<SteadyForcing>(forcing, multiplier, offset);
 }
 
 bool ModelReader::isTimeDependent(std::string const &ncFileName, std::string const &variableName) {

lib/cuascore/src/utilities.cpp
@@ -68,7 +68,6 @@ inline std::string getPETScConfiguration() { return CUAS::vformat("PETSc configu
 inline std::string getNetCDFInformation() { return CUAS::vformat("NetCDF %s.", nc_inq_libvers()); }
 
 std::string version() {
-  char buffer[TEMPORARY_STRING_LENGTH];
   std::string result;
 
   result += CUAS::vformat("CUAS-MPI (branch: %s, tag: %s, commit: %s)", GIT_BRANCH, GIT_TAG, GIT_REV);
@@ -100,7 +99,7 @@ std::string vformat(const char *const zcFormat, ...) {
   // and a functionally reliable call to mock the formatting
   va_list vaArgsCopy;
   va_copy(vaArgsCopy, vaArgs);
-  const int iLen = std::vsnprintf(NULL, 0, zcFormat, vaArgsCopy);
+  auto iLen = std::vsnprintf(nullptr, 0, zcFormat, vaArgsCopy);
   va_end(vaArgsCopy);
 
   // return a formatted string without risking memory mismanagement